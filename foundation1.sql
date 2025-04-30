03b862b4f81a86214ac4b73f45b5ac1e3102845e
hulmen5375 <nicola.hulme@uhnm.nhs.uk>
2025-04-30 15:08:52 +0000

    END OF COURSE

--- foundation1.sql
+++ foundation1.sql
@@ -2,8 +2,8 @@ SELECT
     ps.PatientId
     , ps.AdmittedDate 
     , ps.DischargeDate 
-    , DATEDIFF(day, ps.AdmittedDate, ps.DischargeDate)AS LengthOfStay
-    , DATEADD (Day, -14, ps.AdmittedDate) As ReminderDate
+    --, DATEDIFF(day, ps.AdmittedDate, ps.DischargeDate)AS LengthOfStay
+    --, DATEADD (Day, -14, ps.AdmittedDate) As ReminderDate
     , ps. Hospital
     , ps.Ward 
     , ps.Tariff
@@ -13,10 +13,34 @@ WHERE ps.Hospital IN ('PRUH', 'Oxleas')
 AND ps.Ward LIKE '%Surgery'
 -- AND ps.AdmittedDate >= '2024-02-27'
 -- AND ps.AdmittedDate <= '2024-03-01'
-AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+-- AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+ORDER BY ps.AdmittedDate DESC, ps.PatientId DESC
+
+SELECT 
+COUNT(*) as Numberofpatients 
+,SUM(ps.Tariff) as totaltariff
+,MAX(ps.Tariff) as Maxtariff
+,MIN(ps.tariff) as Mintariff
+, AVG(ps.tariff) as AvgTariff
+FROM PatientStay ps
+GROUP BY ps.Hospital, ps.Ward
+--ORDER BY ps.Hospital, ps.Ward
+ORDER BY Numberofpatients DESC
+
+SELECT
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,ps.Hospital
+    ,dh.HospitalType
+    ,dh.HospitalSize
+FROM
+    PatientStay ps 
+    INNER JOIN DimHospital dh 
+    ON ps.Hospital = dh.Hospital
+  --  WHERE DH.Hospital IS NULL
 
 
-
+SELECT * FROM DimHospitalBad
 
 
 
